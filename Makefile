e600fe11129403366247a356316d773f9deb3315
s-tajima <tajima1989@gmail.com>
2016-10-28 00:07:26 +0900

    fix

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 zip:
 	(cd lambda && zip -r - *) > lambda.zip
-
+	zip cloudformation.zip *.json
+ 
 test:
 	rm -rf /tmp/aws-cfn-lambda-test/
 	cd lambda; python local_test.py
+
+up:
+	aws s3 cp cloudformation.zip s3://aws-cfn-lambda-test/cloudformation.zip
+	aws lambda update-function-code --function-name testFunction --zip-file fileb://lambda.zip
 	
--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,18 +1,18 @@
 from __future__ import print_function
-import git
 import json
 import boto3
 
 print('Loading function')
 
 def lambda_handler(event, context):
-    
-    print("repository_url = " + event['repository_url'])
+    bn = event['bucket_name']
+    path = event['path']
 
-    git.Repo.clone_from(event['repository_url'], '/tmp/aws-cfn-lambda-test/', branch='master')
-
-    client = boto3.client('lambda')
-    response = client.get_function_configuration(FunctionName=context.function_name)
+    l = boto3.client('lambda')
+    response = l.get_function_configuration(FunctionName=context.function_name)
     print(response['Description'])
+    
+    s3 = boto3.resource('s3')
+    s3.meta.client.download_file(bn, path, '/tmp/' + path)
 
-    return event['repository_url']
+    return "Fin."
