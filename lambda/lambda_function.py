6565d917427639ea62bdb9f93e0917c6dda7e9f8
s-tajima <tajima1989@gmail.com>
2016-10-27 23:23:38 +0900

    fix

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -1,12 +1,18 @@
 from __future__ import print_function
 import git
-
 import json
+import boto3
 
 print('Loading function')
 
 def lambda_handler(event, context):
+    
     print("repository_url = " + event['repository_url'])
 
     git.Repo.clone_from(event['repository_url'], '/tmp/aws-cfn-lambda-test/', branch='master')
+
+    client = boto3.client('lambda')
+    response = client.get_function_configuration(FunctionName=context.function_name)
+    print(response['Description'])
+
     return event['repository_url']
